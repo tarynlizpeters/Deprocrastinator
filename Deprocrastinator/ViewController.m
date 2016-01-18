70f0821ee5b2ffd32bc0b33223b3de21477c77b6
Taryn Parker <tarynp@Taryns-MacBook-Air.local>
2016-01-18 14:31:20 -0600

    change color to green on tap

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property NSMutableArray *things;
 @end
 
@@ -25,17 +26,28 @@
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellId"];
+    cell.textLabel.text = [self.things objectAtIndex:indexPath.row];
+    
     return cell;
 }
 
 - (IBAction)onAddButtonPressed:(UIBarButtonItem *)sender {
     
+    if (![self.textField.text isEqualToString:@"" ]) {
+        [self.things addObject:self.textField.text];
+        [self.tableView reloadData];
+        self.textField.text = @"" ;
+        [self.textField resignFirstResponder];
+    }
+    
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@" %ld", (long)indexPath.row);
+    [tableView cellForRowAtIndexPath:indexPath].textLabel.textColor = [UIColor greenColor];
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
